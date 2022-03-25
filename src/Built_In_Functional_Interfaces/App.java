59568ebb38659e0c26ba1fb8163106c7b7534faf
Khulan <68979818+khulanJargalsaikhan@users.noreply.github.com>
2022-03-25 13:26:14 -0400

    Update App.java

--- src/Built_In_Functional_Interfaces/App.java
+++ src/Built_In_Functional_Interfaces/App.java
@@ -20,20 +20,26 @@ public class App {
 				new Car("Jeep", "Wrangler", "Red", 24500)
 				);
 
-
+		// bc they are using same resource(cars list)
+		// once we have lambda down below, we don't need these two methods
 		printCarsPriceRange(cars, 18000, 22000);
 		printCarByColor(cars, "Red");
 
 
+		//we can do 2 methods in one place using lambda
 		System.out.println("Printing cars between price 18000 and 22000");
+		//anonymous class definition
 		printCars(cars, new CarCondition() {
 
 			@Override
 			public boolean test(Car c) {
+				//here we can define own condition
 				return c.getPrice() >= 18000 && c.getPrice() <= 22000;
 			}
 		});
 	
+		//lambda expression for printing CarByColor
+		//this is also polymorphism
 		System.out.println("Printing cars that are blue");
 		printCars(cars, new CarCondition() {
 
@@ -45,11 +51,11 @@ public class App {
 		});
 
 		// this used CarCondition functional interface that we created down below
-//		System.out.println("Printing cars between price 18000 and 22000");
-//		printCars(cars, (c) -> c.getPrice() >= 18000 && c.getPrice() <= 22000);
-//
-//		System.out.println("Print blue cars");
-//		printCars(cars, (c) -> c.getColor().equals("Blue"));
+		System.out.println("Printing cars between price 18000 and 22000");
+		printCars(cars, (c) -> c.getPrice() >= 18000 && c.getPrice() <= 22000);
+
+		System.out.println("Print blue cars");
+		printCars(cars, (c) -> c.getColor().equals("Blue"));
 		
 		
 		
@@ -65,9 +71,11 @@ public class App {
 
 	
 	
-	
+	//creating just one method that able to use lambda and pass their own condition instead of method 1 and 2
+	//CarCondition is up to the user what condition would be
 	public static void printCars(List<Car> cars, CarCondition condition) {
 		for(Car c: cars) {
+			//print only if the value of condition is true
 			if(condition.test(c)) {
 				c.printCar();
 			}
@@ -75,6 +83,7 @@ public class App {
 	}
 
 
+	//method 1
 	public static void printCarsPriceRange(List<Car> cars, int low, int high) {
 		for(Car c: cars) {
 			if(low <= c.getPrice() && c.getPrice() <= high) {
@@ -83,7 +92,7 @@ public class App {
 		}
 	}
 
-
+	//method 2
 	public static void printCarByColor(List<Car> cars, String color) {
 		for(Car c: cars) {
 			if(c.getColor().equals(color)) {
@@ -98,6 +107,7 @@ public class App {
 
 
 	// Using built-in functional interface
+	// check out java.util.function library in oracle for built-in functional interfaces
 	public static void printCars(List<Car> cars, Predicate<Car> predicate) {
 		for(Car c: cars) {
 			if(predicate.test(c)) {
